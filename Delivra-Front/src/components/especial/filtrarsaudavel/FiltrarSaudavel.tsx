d5e14507f94e243113cafdb52254248f1ffa41cc
julio <juliocostadeaguiar@gmail.com>
2026-05-20 18:16:16 -0300

    fix/filtrarsaudavel

--- Delivra-Front/src/components/especial/filtrarsaudavel/FiltrarSaudavel.tsx
+++ Delivra-Front/src/components/especial/filtrarsaudavel/FiltrarSaudavel.tsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from 'react'
+import type Produto from '../../../models/Produto'
+import { buscar } from '../../../services/Service'
+import CardProduto from '../../produto/cardproduto/CardProduto'
+
 export default function FiltrarSaudavel() {
 
+  const [produtos, setProdutos] = useState<Produto[]>([])
 
+  async function buscarProdutos() {
+    await buscar('/produtos/saudaveis', setProdutos)
+  }
 
+  useEffect(() => {
+    buscarProdutos()
+  }, [])
 
+  const produtosSaudaveis = produtos.filter(
+    (produto) => produto.saudavel === true
+  )
 
-    
+  return (
+    <div>
+      <h1>Produtos Saudáveis</h1>
+
+      <div>
+        {produtosSaudaveis.map((produto) => (
+          <CardProduto
+            key={produto.id}
+            produto={produto}
+          />
+        ))}
+      </div>
+    </div>
+  )
 }
